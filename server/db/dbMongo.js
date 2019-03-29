2d7d103607b008ed464830073dbb3a11ed417aa1
Michael Ryan <michaeltheryan@gmail.com>
2019-03-29 15:06:16 -0400

    Refactored and created seeding files for MongoDB/ Mongoose and PostgreSQL
    Seeding file for PostGreSQL has been tested and is
    complete - will perform complete seeding this evening
    after regenerating the 10 million records due to dataset
    generation refinements made over the last 48 hours
    
    Next step is to finish MongoDB/Mongoose seeding script
    and then start speed testing

---
+++ server/db/dbMongo.js
@@ -0,0 +1,73 @@
+﻿var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+mongoose.connect('mongodb://localhost:27017/myapp');
+
+  var movieSchema = new Schema({
+    id: false,
+    _id: Number,
+    title: {
+      type: String,
+      default: null
+    },
+    title_url: String,
+    tmdb_poster_path: String,
+    tmdb_backdrop_path: String
+  });
+
+  movieSchema.Index( { title: 1 } );
+
+  var criticSchema = new Schema({
+    id: false,
+    _id: Number,
+    top_critic: {
+      type: Number,
+      default: null,
+      min: 0,
+      max: 1
+    },
+    name: {
+      type: String,
+      default: null
+    },
+    img_url: String,
+    organization: {
+      type: String,
+      default: null
+    },
+  });
+
+  var reviewSchema = new Schema({
+    id: false,
+    _id: Number,
+    review_date: {
+      type: Date,
+      default: null
+    },
+    fresh: {
+      type: Number,
+      min: 0,
+      max: 1,
+      default: 0
+    },
+    review_text: {
+      type: String,
+      default: null
+    },
+    movie_id: {
+      type: Number,
+      default: null,
+    },
+    critic_id: {
+      type: Number,
+      default: null
+    },
+    score: {
+      type: Number,
+      default: null,
+      min: 0,
+      max: 10
+    },
+  });
+
+  db.Index( { name: 1 } );
