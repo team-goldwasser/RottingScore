e6966953386749e0a423239cc42f3e780cfa869e
Michael Ryan <michaeltheryan@gmail.com>
2019-04-11 17:04:46 -0400

    Create routing for CRUD including GET, POST, PUT, and DELETE
    Modified app.js as needed to refer to new routing
    
    Undid previous changes to API which were unnecessary

--- server/app.js
+++ server/app.js
@@ -6,238 +6,33 @@ const cors = require('cors');
 
 app.use(cors());
 
-app.get('/m/movieinfo/:id', getMovieInfo
-// function (req, res, next) {
-//   if (req.params && req.params['id']) {
-//     db.getMovieInfo(req.params['id'])
-//       .then(function (results) {
-//         if (results[0].id === 284054) {
-//           //black panther
-//           var backdrop = 'https://image.tmdb.org/t/p/w780/6ELJEzQJ3Y45HczvreC3dg0GV5R.jpg';
-//           var poster = 'https://image.tmdb.org/t/p/w342/uxzzxijgPIY7slzFvMotPv8wjKA.jpg';
-//           var video = 'xjDjIWPwcPU';
-//         } else if (results[0].id === 27205) {
-//           //inception
-//           var backdrop = 'https://image.tmdb.org/t/p/w780/s2bT29y0ngXxxu2IA8AOzzXTRhd.jpg';
-//           var poster = 'https://image.tmdb.org/t/p/w342/qmDpIHrmpJINaRKAfWQfftjCdyi.jpg';
-//           var video = 'zp_YGmAoht0';
-//         } else if (results[0].id === 383498) {
-//           //deadpool 2
-//           var backdrop = 'https://image.tmdb.org/t/p/w780/3P52oz9HPQWxcwHOwxtyrVV1LKi.jpg';
-//           var poster = 'https://image.tmdb.org/t/p/w342/to0spRl1CMDvyUbOnbb4fTk3VAd.jpg';
-//           var video = 'dASJGnDzf3s';
-//         } else {
-//           var backdrop = 'http://lorempixel.com/740/290/nightlife/';
-//           var poster = 'http://lorempixel.com/342/513/people';
-//           var video = 'dQw4w9WgXcQ';
-//         }
-//         res.send({id: results[0].id, title: results[0].title, title_url: results[0].title_url, video: video, poster_url:poster, backdrop_url: backdrop });
-//       })
-//       .catch(function (error) {
-//         if (error) {
-//           res.send({id: 0, title: 'not a movie in db', title_url: 'not_a_movie_in_db', video: 'dQw4w9WgXcQ', poster_url:'http://lorempixel.com/342/513/people', backdrop_url: 'http://lorempixel.com/780/439/nightlife/'});
-//         }
-//       });
-//   } else {
-//     res.send({id: 0, title: 'not a movie in db', title_url: 'not_a_movie_in_db', video: 'dQw4w9WgXcQ', poster_url:'http://lorempixel.com/342/513/people', backdrop_url: 'http://lorempixel.com/780/439/nightlife/'});
-//   }
+app.get('/m/movieinfo/:id', getMovieInfobyID);
+app.get('/m/movieinfo/:name', getMovieInfobyName);
 
-);
+app.get('/cr/criticinfo/:name', getCriticbyName);
 
-app.get('/m/movieinfo', getMovieInfobyName
-// function (req, res, next) {
-//   res.send({id: 0, title: 'not a movie in db', title_url: 'not_a_movie_in_db', video: 'dQw4w9WgXcQ', poster_url:'http://lorempixel.com/342/513/people', backdrop_url: 'http://lorempixel.com/780/439/nightlife/'});
-);
+app.get('/cr/tomotometer/:id', getReviewsbyID);
+app.get('/cr/tomotometer/:name', getReviewsbyName);
 
-app.get('/cr/tomotometer/:id', getCriticbyID
-// function (req, res, next) {
+app.get('/cr/topcriticmeter/:id', getTopReviewsbyID);
+app.get('/cr/topcriticmeter/:name', getTopReviewsbyName);
 
-//   if (req.params && req.params['id']) {
-//     var title_url;
-//     var movie_id;
-//     db.getMovieInfo(req.params['id'])
-//       .then(function (results) {
-//         title_url = results[0].title_url;
-//         movie_id = results[0].id;
-//         return db.getTomatoMeter(movie_id);
-//       })
-//       .then(function (results) {
-//         res.send({
-//           id: movie_id,
-//           title_url: title_url,
-//           tomatometer: Math.round(results[0].tomatometer * 1000) / 10, //take the tomatometer out of 1.0 and set it out of 100
-//           avgrating: Math.round(results[0].score_avg*10)/10,
-//           numOfReviews: results[0].review_count,
-//           fresh: Math.floor(results[0].tomatometer * results[0].review_count), //calculuate # of fresh
-//           rotten: (results[0].review_count - Math.floor(results[0].tomatometer * results[0].review_count)), //calculuate # of rotten
-//           randomReview: (results[0].review_text)
-//         });
-//       })
-//       .catch(function (error) {
-//         if (error) {
-//           res.send({
-//             'id': 0,
-//             'title_url': 'error_not_a_title_in_db',
-//             'tomatometer': 0.0,
-//             'avgrating': 0.0,
-//             'numOfReviews': 0,
-//             'fresh': 0,
-//             'rotten': 0,
-//             'randomReview': 'Well, the way they make a shows is, they make one show. That show\'s called a pilot. Then they show that show to the people who make shows, and on the strength of that one show they decide if they\'re going to make more shows. Some pilots get picked and b...'
-//           });
-//         }
-//       });
-//   } else {
-//     res.send({
-//       'id': 0,
-//       'title_url': 'error_not_a_title_in_db',
-//       'tomatometer': 0.0,
-//       'avgrating': 0.0,
-//       'numOfReviews': 0,
-//       'fresh': 0,
-//       'rotten': 0,
-//       'randomReview': 'Well, the way they make a shows is, they make one show. That show\'s called a pilot. Then they show that show to the people who make shows, and on the strength of that one show they decide if they\'re going to make more shows. Some pilots get picked and b...'
-//     });
-//   }
+app.get('/cr/topdozenreviews/:id', get12ReviewsbyID);
+app.get('/cr/topdozenreviews/:name', get12ReviewsbyName);
 
-);
+app.post('/m/movieinfo', createFilm);
+app.delete('/m/movieinfo/:id', deleteFilmbyID);
+app.delete('/m/movieinfo/:name', deleteFilmbyName);
+app.put('/m/movieinfo/:id', updateFilmbyID);
+app.put('/m/movieinfo/:name', updateFilmbyName);
 
-app.get('/cr/tomotometer/', getCriticbyID);
+app.post('/cr/criticinfo', createCritic);
+app.delete('/cr/criticinfo/:name', deleteCritic);
+app.put('/cr/criticinfo/:name', updateCritic);
 
-app.get('/cr/tomotometer:id', getReviewsbyID
-// function (req, res, next) {
-//   res.send({
-//     'id': 0,
-//     'title_url': 'error_not_a_title_in_db',
-//     'tomatometer': 0.0,
-//     'avgrating': 0.0,
-//     'numOfReviews': 0,
-//     'fresh': 0,
-//     'rotten': 0,
-//     'randomReview': 'Well, the way they make a shows is, they make one show. That show\'s called a pilot. Then they show that show to the people who make shows, and on the strength of that one show they decide if they\'re going to make more shows. Some pilots get picked and b...'
-//   });
-);
-
-app.get('/cr/tomotometer', getReviewsbyName)
-
-app.get('/cr/topcriticmeter/:id', getTopReviewsbyID
-// function (req, res, next) {
-
-//   if (req.params && req.params['id']) {
-//     var title_url;
-//     var movie_id;
-//     db.getMovieInfo(req.params['id'])
-//       .then(function (results) {
-//         title_url = results[0].title_url;
-//         movie_id = results[0].id;
-//         return db.getTopCriticMeter(movie_id);
-//       })
-//       .then(function (results) {
-//         res.send({
-//           id: movie_id,
-//           title_url: title_url,
-//           tomatometer: Math.round(results[0].tomatometer * 1000) / 10, //take the tomatometer out of 1.0 and set it out of 100
-//           avgrating: Math.round(results[0].score_avg*10)/10,
-//           numOfReviews: results[0].review_count,
-//           fresh: Math.floor(results[0].tomatometer * results[0].review_count), //calculuate # of fresh
-//           rotten: (results[0].review_count - Math.floor(results[0].tomatometer * results[0].review_count)), //calculuate # of rotten
-//           randomReview: (results[0].review_text)
-//         });
-//       })
-//       .catch(function (error) {
-//         if (error) {
-//           console.log(error);
-//           res.send({
-//             'id': 0,
-//             'title_url': 'error_not_a_title_in_db',
-//             'tomatometer': 0.0,
-//             'avgrating': 0.0,
-//             'numOfReviews': 0,
-//             'fresh': 0,
-//             'rotten': 0,
-//             'randomReview': 'Well, the way they make a shows is, they make one show. That show\'s called a pilot. Then they show that show to the people who make shows, and on the strength of that one show they decide if they\'re going to make more shows. Some pilots get picked and b...'
-//           });
-//         }
-//       });
-//   } else {
-//     res.send({
-//       'id': 0,
-//       'title_url': 'error_not_a_title_in_db',
-//       'tomatometer': 0.0,
-//       'avgrating': 0.0,
-//       'numOfReviews': 0,
-//       'fresh': 0,
-//       'rotten': 0,
-//       'randomReview': 'Well, the way they make a shows is, they make one show. That show\'s called a pilot. Then they show that show to the people who make shows, and on the strength of that one show they decide if they\'re going to make more shows. Some pilots get picked and b...'
-//     });
-//   }
-
-);
-
-app.get('/cr/topcriticmeter', getTopReviewsbyName
-// function (req, res, next) {
-//   res.send({
-//     'id': 0,
-//     'title_url': 'error_not_a_title_in_db',
-//     'tomatometer': 0.0,
-//     'avgrating': 0.0,
-//     'numOfReviews': 0,
-//     'fresh': 0,
-//     'rotten': 0,
-//     'randomReview': 'Well, the way they make a shows is, they make one show. That show\'s called a pilot. Then they show that show to the people who make shows, and on the strength of that one show they decide if they\'re going to make more shows. Some pilots get picked and b...'
-//   });
-);
-
-app.get('/cr/topdozenreviews/:id', function (req, res, next) {
-
-  if (req.params && req.params['id']) {
-    var movie_id = req.params.id;
-    return db.getDozenReviews(movie_id)
-    .then(function (results) {
-      res.send(results);
-    })
-    .catch(function (error) {
-      if (error) {
-        console.log(error);
-        res.send([{
-          'id': 0,
-          'title_url': 'error_not_a_title_in_db',
-          'tomatometer': 0.0,
-          'avgrating': 0.0,
-          'numOfReviews': 0,
-          'fresh': 0,
-          'rotten': 0,
-          'randomReview': 'Well, the way they make a shows is, they make one show. That show\'s called a pilot. Then they show that show to the people who make shows, and on the strength of that one show they decide if they\'re going to make more shows. Some pilots get picked and b...'
-        }]);
-      }
-    });
-  } else {
-    res.send([{
-      'id': 0,
-      'title_url': 'error_not_a_title_in_db',
-      'tomatometer': 0.0,
-      'avgrating': 0.0,
-      'numOfReviews': 0,
-      'fresh': 0,
-      'rotten': 0,
-      'randomReview': 'Well, the way they make a shows is, they make one show. That show\'s called a pilot. Then they show that show to the people who make shows, and on the strength of that one show they decide if they\'re going to make more shows. Some pilots get picked and b...'
-    }]);
-  }
-
-});
-
-app.get('/cr/topdozenreviews', function (req, res, next) {
-  res.send({
-    'id': 0,
-    'title_url': 'error_not_a_title_in_db',
-    'tomatometer': 0.0,
-    'avgrating': 0.0,
-    'numOfReviews': 0,
-    'fresh': 0,
-    'rotten': 0,
-    'randomReview': 'Well, the way they make a shows is, they make one show. That show\'s called a pilot. Then they show that show to the people who make shows, and on the strength of that one show they decide if they\'re going to make more shows. Some pilots get picked and b...'
-  });
-});
+app.post('/rev/reviewinfo', createReview);
+app.delete('/rev/reviewinfo/:id', deleteReview);
+app.put('/rev/reviewinfo/:id', updateReview);
 
 app.use('/assets', express_static('./Scoreboard/dist/Scoreboard/assets/'));
 
--- server/db/dbPostgresQuery.js
+++ server/db/dbPostgresQuery.js
@@ -19,18 +19,8 @@ const getMovieInfobyID = (request, response) => {
 };
 
 const getMovieInfobyName = (request, response) => {
-  const id = parseInt(request.params.id);
-  pool.query('SELECT * FROM movies WHERE id = $1 _______________', [id], (error, results) => {
-    if (error) {
-      throw error
-    }
-    response.status(200).json(results.rows)
-  })
-};
-
-const getCriticbyID = (request, response) => {
-  const id = parseInt(request.params.id);
-  pool.query('SELECT * FROM critics WHERE id = $1', [id], (error, results) => {
+  const filmTitle = parseInt(request.params.name);
+  pool.query('SELECT * FROM movies WHERE newTitle = $1', [filmTitle], (error, results) => {
     if (error) {
       throw error
     }
@@ -39,8 +29,9 @@ const getCriticbyID = (request, response) => {
 };
 
 const getCriticbyName = (request, response) => {
-  const id = parseInt(request.params.id);
-  pool.query('SELECT * FROM critics WHERE id = $1 _______________', [id], (error, results) => {
+  const name = parseInt(request.params.name);
+  pool.query('SELECT * FROM reviews LEFT JOIN critics ON ' +
+    '(reviews.idCrit = critics.idCritic) WHERE critics.name = $1)', [name], (error, results) => {
     if (error) {
       throw error
     }
@@ -59,8 +50,30 @@ const getReviewsbyID = (request, response) => {
 };
 
 const getReviewsbyName = (request, response) => {
+  const filmName = parseInt(request.params.name);
+  pool.query('SELECT * FROM reviews LEFT JOIN movies ON ' +
+    '(reviews.idFilm = movies.newTitle) WHERE movies.newTitle = $1 )', [filmName], (error, results) => {
+    if (error) {
+      throw error
+    }
+    response.status(200).json(results.rows)
+  })
+};
+
+const get12ReviewsbyID = (request, response) => {
   const id = parseInt(request.params.id);
-  pool.query('SELECT * FROM reviews WHERE ______________ = $1', [id], (error, results) => {
+  pool.query('SELECT * FROM reviews WHERE idFilm = $1 LIMIT 12', [id], (error, results) => {
+    if (error) {
+      throw error
+    }
+    response.status(200).json(results.rows)
+  })
+};
+
+const get12ReviewsbyName = (request, response) => {
+  const filmName = parseInt(request.params.name);
+  pool.query('SELECT * FROM reviews LEFT JOIN movies ON ' +
+    '(reviews.idFilm = movies.newTitle) WHERE movies.newTitle = $1 LIMIT 12)', [filmName], (error, results) => {
     if (error) {
       throw error
     }
@@ -70,7 +83,8 @@ const getReviewsbyName = (request, response) => {
 
 const getTopReviewsbyID = (request, response) => {
   const id = parseInt(request.params.id);
-  pool.query('SELECT * FROM reviews WHERE idFilm = $1 AND __________', [id], (error, results) => {
+  pool.query('SELECT * FROM reviews WHERE idFilm = $1 LEFT JOIN critics ON ' +
+    '(reviews.idCrit = critics.idCritic) WHERE critics.topCritic = $2 )', [id, 1], (error, results) => {
     if (error) {
       throw error
     }
@@ -79,8 +93,9 @@ const getTopReviewsbyID = (request, response) => {
 };
 
 const getTopReviewsbyName = (request, response) => {
-  const id = parseInt(request.params.id);
-  pool.query('SELECT * FROM reviews WHERE idFilm = $1 AND __________', [id], (error, results) => {
+  const filmName = parseInt(request.params.name);
+  pool.query('SELECT * FROM reviews WHERE newTitle = $1 LEFT JOIN critics ON ' +
+    '(reviews.idCrit = critics.idCritic) WHERE critics.topCritic = $2 )', [filmName, 1], (error, results) => {
     if (error) {
       throw error
     }
@@ -89,107 +104,138 @@ const getTopReviewsbyName = (request, response) => {
 };
 
 const createFilm = (request, response) => {
-  const { newID, newTitle, newTitleURL, newPoster, newBackdrop } = request.body
-  pool.query('INSERT INTO movies (newID, newTitle, newTitleURL, newPoster, newBackdrop) ' +
-    'VALUES ($1, $2, $3, $4, $5)', [newID, newTitle, newTitleURL, newPoster, newBackdrop],
+  const { newTitle, newTitleURL, newPoster, newBackdrop } = request.body
+  pool.query('INSERT INTO movies (newTitle, newTitleURL, newPoster, newBackdrop) ' +
+    'VALUES ($1, $2, $3, $4)', [newTitle, newTitleURL, newPoster, newBackdrop],
     (error, results) => {
       if (error) {
         throw error
     }
-    response.status(201).send(`Film added with ID: ${result.insertId}`)
+    response.status(201).send(`${results.newTitle} has been added to film database!`)
   })
 }
 
-const updateFilm = (request, response) => {
+const updateFilmbyID = (request, response) => {
   const id = parseInt(request.params.id)
-  const { newID, newTitle, newTitleURL, newPoster, newBackdrop } = request.body
+  const { newTitle, newTitleURL, newPoster, newBackdrop } = request.body
 
   pool.query(
-    'UPDATE movies SET newID = $1, newTitle = $2, newTitleURL = $3, newPoster = $4, newBackdrop = $5',
-    [newID, newTitle, newTitleURL, newPoster, newBackdrop],
+    'UPDATE movies SET newTitle = $2, newTitleURL = $3, newPoster = $4, newBackdrop = $5 ' +
+      'WHERE newID = $1',
+    [id, newTitle, newTitleURL, newPoster, newBackdrop],
     (error, results) => {
       if (error) {
         throw error
       }
-      response.status(200).send(`Film modified with ID: ${id}`)
+      response.status(200).send(`${results.newTitle} has been modified.`)
     }
   )
 }
 
-const deleteFilm = (request, response) => {
-  const id = parseInt(request.params.id)
-
-  pool.query('DELETE FROM movies WHERE id = $1', [id], (error, results) => {
-    if (error) {
-      throw error
-    }
-    response.status(200).send(`Film deleted with ID: ${id}`)
-  })
-}
-
-const createCritic = (request, response) => {
-  const { idCritic, topCritic, name, newImage, org } = request.body
-  pool.query('INSERT INTO critics (idCritic, topCritic, name, newImage, org) ' +
-    'VALUES ($1, $2, $3, $4, $5)', [idCritic, topCritic, name, newImage, org],
-    (error, results) => {
-      if (error) {
-        throw error
-    }
-    response.status(201).send(`Critic added with ID: ${result.insertId}`)
-  })
-}
-
-const updateCritic = (request, response) => {
-  const idCritic = parseInt(request.params.id)
-  const { idCritic, topCritic, name, newImage, org } = request.body
+const updateFilmbyName = (request, response) => {
+  const filmName = parseInt(request.params.name)
+  const { newTitle, newTitleURL, newPoster, newBackdrop } = request.body
 
   pool.query(
-    'UPDATE critics SET idCritic = $1, topCritic = $2, name = $3, newImage = $4, org = $5',
-    [idCritic, topCritic, name, newImage, org],
+    'UPDATE movies SET newTitle = $2, newTitleURL = $3, newPoster = $4, newBackdrop = $5 ' +
+      'WHERE newTitle = $1',
+    [filmName, newTitle, newTitleURL, newPoster, newBackdrop],
     (error, results) => {
       if (error) {
         throw error
       }
-      response.status(200).send(`Critic modified with ID: ${id}`)
+      response.status(200).send(`${results.newTitle} has been modified.`)
+    }
+  )
+}
+
+const deleteFilmbyID = (request, response) => {
+  const id = parseInt(request.params.id)
+
+  pool.query('DELETE * FROM movies WHERE newID = $1', [id], (error, results) => {
+    if (error) {
+      throw error
+    }
+    response.status(200).send(`${results.newTitle} has been deleted from film database!`)
+  })
+}
+
+const deleteFilmbyName = (request, response) => {
+  const filmName = parseInt(request.params.name)
+
+  pool.query('DELETE * FROM movies WHERE newTitle = $1', [filmName], (error, results) => {
+    if (error) {
+      throw error
+    }
+    response.status(200).send(`${results.newTitle} has been deleted from film database!`)
+  })
+}
+
+const createCritic = (request, response) => {
+  const { topCritic, name, newImage, org } = request.body
+  pool.query('INSERT INTO critics (topCritic, name, newImage, org) ' +
+    'VALUES ($1, $2, $3, $4)', [topCritic, name, newImage, org],
+    (error, results) => {
+      if (error) {
+        throw error
+    }
+    response.status(201).send(`${results.name} has been added to critics database!`)
+  })
+}
+
+const updateCritic = (request, response) => {
+  const nameCritic = parseInt(request.params.name)
+  const { topCritic, name, newImage, org } = request.body
+
+  pool.query(
+    'UPDATE critics SET topCritic = $2, name = $3, newImage = $4, org = $5' +
+      'WHERE name = $1',
+    [nameCritic, topCritic, name, newImage, org],
+    (error, results) => {
+      if (error) {
+        throw error
+      }
+      response.status(200).send(`${results.name}'s profile has been modified.`)
     }
   )
 }
 
 const deleteCritic = (request, response) => {
-  const idCritic = parseInt(request.params.id)
+  const nameCritic = parseInt(request.params.name)
 
-  pool.query('DELETE FROM critics WHERE idCritic = $1', [id], (error, results) => {
+  pool.query('DELETE FROM critics WHERE name = $1', [nameCritic], (error, results) => {
     if (error) {
       throw error
     }
-    response.status(200).send(`Film deleted with ID: ${id}`)
+    response.status(200).send(`${results.name} has been deleted.`)
   })
 }
 
 const createReview = (request, response) => {
-  const { newID, newTitle, newTitleURL, newPoster, newBackdrop } = request.body
-  pool.query('INSERT INTO reviews (reviewID, newDate, fresh, reviewText, idFilm, idCrit, rating) ' +
-    'VALUES ($1, $2, $3, $4, $5, $6, $7)', [reviewID, newDate, fresh, reviewText, idFilm, idCrit, rating],
+  const { newDate, fresh, reviewText, idFilm, idCrit, rating } = request.body
+  pool.query('INSERT INTO reviews (newDate, fresh, reviewText, idFilm, idCrit, rating) ' +
+    'VALUES ($1, $2, $3, $4, $5, $6)', [newDate, fresh, reviewText, idFilm, idCrit, rating],
     (error, results) => {
       if (error) {
         throw error
     }
-    response.status(201).send(`Review added with ID: ${result.insertId}`)
+    response.status(201).send(`Review added for ${results.newTitle}`)
   })
 }
 
 const updateReview = (request, response) => {
   const reviewID = parseInt(request.params.id)
-  const { reviewID, newDate, fresh, reviewText, idFilm, idCrit, rating } = request.body
+  const { newDate, fresh, reviewText, idFilm, idCrit, rating } = request.body
 
   pool.query(
-    'UPDATE reviews SET reviewID = $1, newDate = $2, fresh = $3, reviewText = $4, idFilm = $5, idCrit = $6, rating = $7',
+    'UPDATE reviews SET newDate = $2, fresh = $3, reviewText = $4, idFilm = $5, idCrit = $6, ' +
+      'rating = $7 WHERE reviewID = $1',
     [reviewID, newDate, fresh, reviewText, idFilm, idCrit, rating],
     (error, results) => {
       if (error) {
         throw error
       }
-      response.status(200).send(`Review modified with ID: ${id}`)
+      response.status(200).send(`${results.newTitle} review modified`)
     }
   )
 }
@@ -197,25 +243,29 @@ const updateReview = (request, response) => {
 const deleteReview = (request, response) => {
   const reviewID = parseInt(request.params.id)
 
-  pool.query('DELETE FROM reviews WHERE reviewID = $1', [id], (error, results) => {
+  pool.query('DELETE FROM reviews WHERE reviewID = $1', [reviewID], (error, results) => {
     if (error) {
       throw error
     }
-    response.status(200).send(`Review deleted with ID: ${id}`)
+    response.status(200).send(`${results.newTitle} review deleted`)
   })
 }
 
 module.exports = {
   getMovieInfobyID,
   getMovieInfobyName,
-  getCriticbyID,
   getCriticbyName,
   getReviewsbyID,
   getReviewsbyName,
+  getTopReviewsbyName,
   getTopReviewsbyID,
+  get12ReviewsbyID,
+  get12ReviewsbyName,
   createFilm,
-  updateFilm,
-  deleteFilm,
+  updateFilmbyID,
+  updateFilmbyName,
+  deleteFilmbyID,
+  deleteFilmbyName,
   createCritic,
   updateCritic,
   deleteCritic,
